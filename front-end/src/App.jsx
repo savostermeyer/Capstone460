071db92b7f0e5576dfb0b8a7b9fa53b926238d59
Damian Williams <damianwil325@yahoo.com>
2026-01-03 18:38:20 -0500

    working jsx default files

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Home() {
+  return <h1>Home</h1>;
 }
 
-export default App
+function Upload() {
+  return <h1>Upload</h1>;
+}
+
+function Reports() {
+  return <h1>Reports</h1>;
+}
+
+function Login() {
+  return <h1>Login</h1>;
+}
+
+function Team() {
+  return <h1>Team</h1>;
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/team" element={<Team />} />
+    </Routes>
+  );
+}
