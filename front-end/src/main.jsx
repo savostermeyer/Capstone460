98e34e947371914a394ac68a6cead1d319c8daf9
Damian Williams <damianwil325@yahoo.com>
2026-01-03 22:22:06 -0500

    chatbot widget created, css styling for widget fixed

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/styles.css";
 import "./styles/upload.css";
+import "./styles/chatbot.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
