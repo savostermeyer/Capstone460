b87bde9036edce9a600f2ac3d2e21a6db12396b1
Damian Williams <damianwil325@yahoo.com>
2026-01-03 21:11:59 -0500

    migrated Upload into jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,13 +2,14 @@
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx"
+import Footer from "./components/Footer.jsx";
 
 import Home from "./pages/Home.jsx";
 import Upload from "./pages/Upload.jsx";
 import Reports from "./pages/Reports.jsx";
 import Login from "./pages/Login.jsx";
 import Team from "./pages/Team.jsx";
+import About from "./pages/About.jsx";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/reports" element={<Reports />} />
         <Route path="/login" element={<Login />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />
